008ff3232e386c88b910a9feeb5330e7157733f7
Nikolas <9225407@gmail.com>
2024-02-24 16:57:42 +0300

    ru/en languages test

---
+++ src/main/java/org/nickz/spring/http/controller/rest/MessageRestController.java
@@ -0,0 +1,27 @@
+package org.nickz.spring.http.controller.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+@RequiredArgsConstructor
+public class MessageRestController {
+
+    private final MessageSource messageSource;
+
+    @GetMapping
+    public String getMessage(@RequestParam("key") String key,
+                             @RequestParam("lang") String language) {
+
+        return messageSource.getMessage(key, null, null, new Locale(language));
+
+    }
+}
